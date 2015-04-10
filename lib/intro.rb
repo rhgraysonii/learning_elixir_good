36175c2b5cca6084886573a39c38899bed8a20a0
Robert H Grayson II <bobbygrayson@gmail.com>
2015-04-10 16:41:26 -0400

    add ruby version of intro for comparison

---
+++ lib/intro.rb
@@ -0,0 +1,33 @@
+a = 1
+b = 1.0
+c = 0x1
+d = true
+
+puts a ==  b # true
+# puts a === b # doesnt exist
+puts a ==  c # true
+# puts a === c # doesnt exist
+puts a ==  d # false
+# puts a === d # doesnt exist
+
+quotient = a / a # 1
+
+# in Ruby we get a fixnum, no forced float unless
+# it is implicitly cast from one of the args, or
+# if we were to call .to_f for some reason
+
+puts quotient
+
+string = "this"
+symbol = :this
+
+puts string
+puts symbol 
+
+puts string == symbol # false
+
+diff = Proc.new { |a, b| a - b }
+# Anonymous functions behave nearly identically
+
+puts diff.call(5, 3)
+# but we must call .call, rather than Elixir's dot notatoin.
