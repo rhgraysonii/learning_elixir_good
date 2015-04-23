b3a69ef638f5ec8296a38bde693b1d9c3161cf3d
Robert H Grayson II <bobbygrayson@gmail.com>
2015-04-23 16:37:54 -0400

    start anon functions section

--- lib/elixir/anonymous_functions.exs
+++ lib/elixir/anonymous_functions.exs
@@ -1,4 +1,24 @@
 # Anonymous functions are useful and fun
 
-add = fn a, b -> a + b
+sum = 10
+
+# this is not breaking immutability. Internally it looks
+# at the original value and binds a new value. Anything
+# that uses sum prior to this will reference the original
+
+sum = 12
+
+sum_val = fn -> IO.puts "sum was: #{sum}" end
+
+sum = 100
+
+sum_val.()
+# call with .() for anon functions
+
+# => sum was 12
+# :ok
+
+# the .() notation clears ambiguity between calling
+# the function, and just being able to call thing.self
+# over thing.self()
 
