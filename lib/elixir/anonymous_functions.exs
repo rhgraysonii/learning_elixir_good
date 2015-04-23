925b0add48ed6891ff56b5f8e6cdb1ad453c82a0
Robert H Grayson II <bobbygrayson@gmail.com>
2015-04-23 18:30:50 -0400

    finish anon functions

--- lib/elixir/anonymous_functions.exs
+++ lib/elixir/anonymous_functions.exs
@@ -22,3 +22,33 @@ sum_val.()
 # the function, and just being able to call thing.self
 # over thing.self()
 
+add = fn x, y -> x + y end
+
+# last thing evalled is always returned. Elixir has no return
+# keyword to utilize
+
+subtract = fn x, y -> x - y end
+
+perform_calculation = fn(a, b, func ->
+  func.(a, b))
+end
+
+a = perform_calculation.(10, 10, add)
+IO.puts a
+# => 20
+# as you can see, functions are high class citizens here
+
+# shorthand functions are a thing
+b = Enum.map [2, 4, 6], &(&1 *2)
+c = Enum.map [2, 4, 6], fn i -> i * 2 end
+res = b == c
+IO.puts res
+# we hass the ampersand to denote the area and arg to save space for one liners
+
+IO.puts b
+
+list = ["test", 1, "red", 4]
+another_list = Enum.filter list, fn element -> element end
+number_list = Enum.filter list, &is_number(&l)
+# => [1, 4]
+
